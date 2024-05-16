61b43bee7066cf9433a84683d3d943b762d82e27
Sadra Saharkhiz <sarkh200@gmail.com>
2024-05-16 01:04:33 -0400

    gavity works

--- WindowManagement/ProcessThread.cs
+++ WindowManagement/ProcessThread.cs
@@ -6,10 +6,13 @@ namespace Physics_Window_Manager.WindowManagement
 	{
 		public static void Process(ProcessData data)
 		{
-			bool isGrounded = false;
+
+			(int x, int y) point = (int.MinValue, int.MinValue);
+			DateTime clickStart = DateTime.MinValue;
 			int [] velocityOptions = [-10, 10];
 			Random random = new();
-			data.Velocity = (0/*velocityOptions [random.Next(velocityOptions.Length)]*/, velocityOptions [random.Next(velocityOptions.Length)]);
+			data.Velocity = (velocityOptions [random.Next(velocityOptions.Length)], velocityOptions [random.Next(velocityOptions.Length)]);
+
 			try
 			{
 				_ = data.DataProcess.HasExited;
@@ -18,23 +21,40 @@ namespace Physics_Window_Manager.WindowManagement
 			{
 				return;
 			}
+
+			ProcessData.RECT borders = new()
+			{
+				Left = 8,
+				Top = 0,
+				Right = 8,
+				Bottom = 8
+			};
 			while (!data.DataProcess.HasExited)
 			{
-				ProcessData.RECT borders = new()
-				{
-					Left = 8,
-					Top = 0,
-					Right = 8,
-					Bottom = 8
-				};
-
 				if (data.StateOfWindow != ProcessData.WindowState.Maximized)
 				{
-					Calculate.CalculateWindowPosition(data.WindowPos, data.Velocity, out (int x, int y) windowPos);
-					Calculate.CalculateCollisions(windowPos, data.Velocity, data.WindowDims, data.ScreenDims, borders, 3, out windowPos, out data.Velocity, out isGrounded);
-					MoveWindow.SetWindowPosition(data.DataProcess, windowPos);
+					if (!(Calculate.IsPointWithinRect(Cursor.Position, data.WindowRect) && (Control.MouseButtons & MouseButtons.Left) != 0))
+					{
+						Calculate.CalculateWindowPosition(data.WindowPos, data.Velocity, out (int x, int y) windowPos);
+						Calculate.CalculateCollisions(windowPos, data.Velocity, data.WindowDims, data.ScreenDims, borders, 3, out windowPos, out data.Velocity, out bool isGrounded);
+						MoveWindow.SetWindowPosition(data.DataProcess, windowPos);
+						if (!isGrounded)
+						{
+							data.Velocity.y += 1;
+						}
+						else
+						{
+							if (data.Velocity.x > 0)
+							{
+								data.Velocity.x -= 1;
+							}
+							else if (data.Velocity.x < 0)
+							{
+								data.Velocity.x += 1;
+							}
+						}
+					}
 				}
-				if (!isGrounded) data.Velocity.y += 1;
 				Thread.Sleep(1);
 			}
 		}
