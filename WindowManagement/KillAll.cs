9dfbc4f852a01a91d812ebddf87c8bc7f15d4106
Sadra Saharkhiz <sarkh200@gmail.com>
2024-05-16 01:14:42 -0400

    changed process name

--- WindowManagement/KillAll.cs
+++ WindowManagement/KillAll.cs
@@ -2,17 +2,17 @@
 
 namespace Physics_Window_Manager.WindowManagement
 {
-    internal class KillAll
-    {
-        public static void Kill()
-        {
-            foreach (Process p in Process.GetProcesses())
-            {
-                if (p.ProcessName == "dvdify")
-                {
-                    p.Kill();
-                }
-            }
-        }
-    }
+	internal class KillAll
+	{
+		public static void Kill()
+		{
+			foreach (Process p in Process.GetProcesses())
+			{
+				if (p.ProcessName == "gravify")
+				{
+					p.Kill();
+				}
+			}
+		}
+	}
 }
